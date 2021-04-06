8f6b1b79b0a4aff6319e59489aa21a9b466140a6
Matthew Rawlings <matthew.rawlings6@gmail.com>
2021-04-06 19:21:09 +0200

    testing api calls and returns

--- src/dom.js
+++ src/dom.js
@@ -1,13 +1,17 @@
-import getWeather from './weather';
+import { getWeatherTemp, getWeatherFeelsLike } from './weather';
 
 const renderWeather = () => {
   const form = document.querySelector('.search-form');
   // const weatherName = document.querySelector('.weather-name');
   const weatherTemp = document.querySelector('.weather-temp');
+  const weatherFeelsLike = document.querySelector('.weather-feels-like');
 
   const location = form.elements[0].value;
-  getWeather(location).then((temp) => {
+  getWeatherTemp(location).then((temp) => {
     weatherTemp.textContent = temp;
+  });
+  getWeatherFeelsLike(location).then((temp) => {
+    weatherFeelsLike.textContent = temp;
   });
 };
 
--- src/weather.js
+++ src/weather.js
@@ -3,7 +3,7 @@
  * @param {string} location - location searched by user
  * @returns {void} - calls renderWeather() with relevant location
  */
-const getWeather = (location) => {
+const getWeatherTemp = (location) => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=490f24bcbc3a2ee5cb3e70d10b15bfab`;
 
   return fetch(url, { mode: 'cors' })
@@ -14,4 +14,15 @@ const getWeather = (location) => {
     });
 };
 
-export default getWeather;
+const getWeatherFeelsLike = (location) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=490f24bcbc3a2ee5cb3e70d10b15bfab`;
+
+  return fetch(url, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => response.main.feels_like)
+    .catch((error) => {
+      console.log(`Location not found: ${error}`);
+    });
+};
+
+export { getWeatherTemp, getWeatherFeelsLike };
